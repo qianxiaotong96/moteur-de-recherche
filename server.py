82737043d4eb118ecc80f6820462ffcd78303f76
qianxiaotong96 <56349129+qianxiaotong96@users.noreply.github.com>
2020-04-11 11:01:11 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 #-*- coding:utf-8 -*-
 import http.server
-import socketserver 
 
 port = 80
 address = ("",port)
 
-handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(address,handler)
+server = http.server.HTTPServer
+
+handler = http.server.CGIHTTPRequestHandler
+handler.cgi_directories = ["/"]
+
+httpd = server(address,handler)
 
 print(f"Serveur démarré sur le PORT {port}")
 
 httpd.serve_forever()
+

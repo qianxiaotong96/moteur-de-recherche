26c32a01686dbe56bd41cd601bf91424892efb1a
qianxiaotong96 <56349129+qianxiaotong96@users.noreply.github.com>
2020-04-25 21:48:11 +0200

    Update pageB.py

--- pageB.py
+++ pageB.py
@@ -1,32 +1,27 @@
-#!/usr/bin/python3.8
-#incoding:utf-8
+#!/usr/bin/python
+#coding:utf-8
 import cgi
-import cgitb
-
-cgitb.enable()
-form = cgi.FieldStorage()
-
-if form.getvalue("cherche"):
-	username = form.getvalue("cherche")
-else:
-	raise Exception("Pseudo non transmis")
-
 
 print("Content-type:text/html;charset=utf-8\n")
 
 html = """<!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Resultat</title>
+	<title>Cherche</title>
 	<style type="text/css">
 	
-		span{
+		h1{
 			font-family: Georgia;
-			font-weight: bold;
-			font-size: 20px;
 			text-shadow:#f3f3f3 1px 1px 0px, #b2b2b2 1px 2px 0;
-			color: deepskyblue;	
+			color: deepskyblue;
+			position: absolute;
+			top: 40%;
+			left: 50%;
+			-webkit-transform: translate(-50%, -50%);
+			-moz-transform: translate(-50%, -50%);
+			-ms-transform: translate(-50%, -50%);
+			-o-transform: translate(-50%, -50%);
+			transform: translate(-50%, -50%);		
 		}
 
 		i{
@@ -53,72 +48,34 @@ html = """<!DOCTYPE html>
 		}
 
 		.recherche{
-			width:100%; 
-			position: absolute;
-			top: 5%;
-			left: 50%;
-			-webkit-transform: translate(-50%, -50%);
-			-moz-transform: translate(-50%, -50%);
+			width:100%; text-align:center;
+				position: absolute;
+				top: 55%;
+				left: 50%;
+				-webkit-transform: translate(-50%, -50%);
+				-moz-transform: translate(-50%, -50%);
 		}
 
-		#resultat{
-			margin-left:20px;
-		}
 
-		p{
-			font-size:small;
-		}
-
-		</style>
+	</style>
 </head>
 <body>
+	<script type="text/javascript">
+		function jump(){
+			if(document.getElementById('text').value)
+				document.form.action = "pageB.py";
+			else
+				document.form.action = "pageA.py";
+		}
+	</script>
 	<div class="all">
-	<form method="post" action="pageB.py" class="recherche"><span>Recherche <i>Gr6  </i></span><input id="text" type="text" name="cherche" placeholder="entrez ce que vous voulez chercher"><input id="submit" type="submit" value="Valider">
+		<h1> Recherche <i>Gr6</i></h1>
+	<form name="form" method="post" action="" class="recherche">
+		<input id="text" type="text" name="cherche" placeholder="entrez ce que vous voulez chercher"><input id="submit" type="submit" value="valider" onclick="jump()">
 	</form>
-	</div>
-	</br>
-	</br>
-	</br>
-	<hr>
-"""
-print(html)
-print(f"vous cherchez : {username}")
-nbre = 6
-print(f" (environ {nbre} résultats ont été trouvés)")
-html2="""
-	<div id="resultat">
-	<div>
-	<a href="#">Resultat1 </a>
-	<p> Contenu1 </p>
-	</div>
-	<div>
-	<a href="#">Resultat2 </a>
-	<p> Contenu2 </p>
-	</div>
-	<div>
-	<a href="#">Resultat3 </a>
-	<p> Contenu3 </p>
-	</div>
-	<div>
-	<a href="#">Resultat4 </a>
-	<p> Contenu4 </p>
-	</div>
-	<div>
-	<a href="#">Resultat5 </a>
-	<p> Contenu5 </p>
-	</div>
-	<div>
-	<a href="#">Resultat6 </a>
-	<p> Contenu6 </p>
-	</div>
 	</div>
 </body>
 </html>
 """
 
-print(html2)
-
-
-
-
-
+print(html)
